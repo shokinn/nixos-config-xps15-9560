b18b8024ad88795882de97c5da50b481141906fd
Philip Hennning <mail@philip-henning.com>
2017-03-23 22:44:16 +0100

    Delete unneeded environment variables

--- env-configuration.nix
+++ env-configuration.nix
@@ -4,7 +4,5 @@
 
 {
   environment.sessionVariables = {
-    PANEL_FIFO = "/tmp/panel-fifo"; # Tempfile for panel-fifo. Needed for bspwm!
-    PATH = "$PATH:/etc/nixos/bspwm/bspwm_panel/"; # Add path to bspwm_panel dir Needed for bspwm!
   };
 }
